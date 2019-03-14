481e30ccc1845aa167616261fb1376f7f4548209
Grenysherg <grenysherg@gmail.com>
2019-03-14 13:33:06 +0300

    add validation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,57 @@
-import rebuildRem from "./rebuild-rem";
+import IMask from "imask";
 
-console.log(true);
-const a = 2;
-rebuildRem();
+const submitDisabledClassName = "button--disabled";
+const inputCorrectClassName = "input--correct";
+const inputErrorClassName = "input--error";
+const inputFiledClassName = "js-form-input-field";
+
+const formElement = document.querySelector(".js-form");
+const telElement = formElement.querySelector(".js-form-tel");
+const submitElement = formElement.querySelector(".js-form-submit");
+
+new IMask(telElement, {
+  mask: "{8}(000) 000-00-00"
+});
+
+submitElement.classList.add(submitDisabledClassName);
+
+formElement.addEventListener(
+  "focus",
+  event => {
+    const element = event.target;
+
+    if (element.classList.contains(inputFiledClassName)) {
+      event.target.parentNode.classList.remove(
+        inputCorrectClassName,
+        inputErrorClassName
+      );
+    }
+  },
+  true
+);
+
+formElement.addEventListener(
+  "blur",
+  event => {
+    const element = event.target;
+
+    if (element.classList.contains(inputFiledClassName)) {
+      const elementContainer = element.parentNode;
+
+      if (element.checkValidity()) {
+        elementContainer.classList.add(inputCorrectClassName);
+        elementContainer.classList.remove(inputErrorClassName);
+      } else {
+        elementContainer.classList.add(inputErrorClassName);
+        elementContainer.classList.remove(inputCorrectClassName);
+      }
+
+      if (formElement.checkValidity()) {
+        submitElement.classList.remove(submitDisabledClassName);
+      } else {
+        submitElement.classList.add(submitDisabledClassName);
+      }
+    }
+  },
+  true
+);
--- src/scripts/rebuild-rem.js
+++
@@ -1,3 +0,0 @@
-export default () => {
-    const b = 1;
-}
